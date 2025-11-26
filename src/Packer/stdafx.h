94a424c678bcb11af91882957defe0ab3dbd8b79
papamikeskilo-bit <papamikeskilo@gmail.com>
2025-11-26 01:32:06 -0300

    Inline UI sources into ClickerDll

--- src/Packer/stdafx.h
+++ src/Packer/stdafx.h
@@ -8,7 +8,13 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <intrin.h>
+#include <cstring>
+#include <cstdlib>
+#include <algorithm>
+
+#if defined(_MSC_VER) && _MSC_VER < 1900
 #pragma intrinsic(strlen,strcpy,strcat,strcmp,memcpy,memset,memcmp)
+#endif
 
 
 #include "targetver.h"
